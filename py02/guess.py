b4f3b0aee3b06905bd5cb1e8e50efe1a97376440
HLQ1102 <1102144757@qq.com>
2018-12-08 09:25:20 +0800

    new

--- py02/guess.py
+++ py02/guess.py
@@ -1,14 +1,13 @@
 import random
 
 number = random.randint(1, 100)
-a= int(input('猜数（1-100）：'))
-for
-if a > number:
-    print('猜大了！')
-    print('我是：', number)
-elif a < number:
-    print('猜小了！')
-    print('我是：', number)
-else:
-    print('猜对了！，您真牛')
-    print('我是：', number)
+# a= int(input('猜数（1-100）：'))
+while 0 > -1:
+    a = int(input('猜数（1-100）：'))
+    if a > number:
+        print('猜大了！')
+    elif a < number:
+        print('猜小了！')
+    else:
+        print('猜对了！，您真牛')
+        break
